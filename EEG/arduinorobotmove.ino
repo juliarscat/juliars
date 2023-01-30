0c0ae9bc0d4242a31b54f0c1fe29d9d82e069262
juliarscat <123875621+juliarscat@users.noreply.github.com>
2023-01-30 10:37:23 +0100

    Create arduinorobotmove.ino

---
+++ EEG/arduinorobotmove.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11); // RX | TX
+
+void setup() {
+  BTSerial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (BTSerial.available()){
+    int data = BTSerial.read();
+
+    if (data == 'w') {
+      // move forward
+    } else if (data == 's') {
+      // move backward
+    } else if (data == 'a') {
+      // turn left
+    } else if (data == 'd') {
+      // turn right
+    }
+  }
+}
