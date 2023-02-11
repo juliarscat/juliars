18c85fc956e5e29f8365cc0023a7521b729dde49
juliarscat <123875621+juliarscat@users.noreply.github.com>
2023-02-11 20:31:28 +0100

    Update exceldatacsv.vb

--- various/exceldatacsv.vb
+++ various/exceldatacsv.vb
@@ -1,65 +1,56 @@
-'Excel macro to import data from a CSV file, then create an index and get the data to create a sheet for every CSV'
 Sub ImportCSVFiles()
-    Dim folderPath As String
-    Dim fileName As String
-    Dim ws As Worksheet
-    Dim indexSheet As Worksheet
-    Dim lastRow As Long
+Dim folderPath As String
+Dim fileName As String
+Dim ws As Worksheet
+Dim indexSheet As Worksheet
+Dim lastRow As Long
+Dim wb As Workbook
+'Set the folder path where the CSV files are located
+folderPath = "C:\CSVFiles\"
 
-    'Set the folder path where the CSV files are located
-    folderPath = "C:\CSVFiles\"
+'Check if the index sheet already exists, if not, create it
+On Error Resume Next
+Set indexSheet = ThisWorkbook.Sheets("Index")
+On Error GoTo 0
+If indexSheet Is Nothing Then
+    Set indexSheet = ThisWorkbook.Sheets.Add
+    indexSheet.Name = "Index"
+End If
 
-    'Check if the index sheet already exists, if not, create it
+'Loop through all the files in the folder
+fileName = Dir(folderPath & "*.csv")
+Do While fileName <> ""
+    'Check if the sheet already exists, if not, create it
     On Error Resume Next
-    Set indexSheet = ThisWorkbook.Sheets("Index")
+    Set ws = ThisWorkbook.Sheets(Replace(fileName, ".csv", ""))
     On Error GoTo 0
-    If indexSheet Is Nothing Then
-        Set indexSheet = ThisWorkbook.Sheets.Add
-        indexSheet.Name = "Index"
+    If ws Is Nothing Then
+        'Import the data from the CSV file into a new sheet
+        Set wb = Workbooks.Open(folderPath & fileName)
+        wb.Sheets(1).Copy After:=ThisWorkbook.Sheets(ThisWorkbook.Sheets.Count)
+        wb.Close False
+        Set ws = ThisWorkbook.Sheets(ThisWorkbook.Sheets.Count)
+        ws.Name = Replace(fileName, ".csv", "")
     End If
 
-    'Loop through all the files in the folder
-    fileName = Dir(folderPath & "*.csv")
-    Do While fileName <> ""
-        'Import the data from the CSV file into a new sheet
-        With ActiveSheet.QueryTables.Add(Connection:="TEXT;" & folderPath & fileName, Destination:=Range("A1"))
-            .Name = Replace(fileName, ".csv", "")
-            .FieldNames = True
-            .RowNumbers = False
-            .FillAdjacentFormulas = False
-            .PreserveFormatting = True
-            .RefreshOnFileOpen = False
-            .RefreshStyle = xlInsertDeleteCells
-            .SavePassword = False
-            .SaveData = True
-            .AdjustColumnWidth = True
-            .RefreshPeriod = 0
-            .TextFilePromptOnRefresh = False
-            .TextFilePlatform = 437
-            .TextFileStartRow = 1
-            .TextFileParseType = xlDelimited
-            .TextFileTextQualifier = xlTextQualifierDoubleQuote
-            .TextFileConsecutiveDelimiter = False
-            .TextFileTabDelimiter = True
-            .TextFileSemicolonDelimiter = False
-            .TextFileCommaDelimiter = True
-            .TextFileSpaceDelimiter = False
-            .TextFileColumnDataTypes = Array(1, 1, 1, 1, 1, 1)
-            .TextFileTrailingMinusNumbers = True
-            .Refresh BackgroundQuery:=False
-        End With
+    'Add the date and time the file was imported to the index sheet
+    lastRow = indexSheet.Cells(indexSheet.Rows.Count, "A").End(xlUp).Row + 1
+    indexSheet.Cells(lastRow, "A").Value = Replace(fileName, ".csv", "")
+    indexSheet.Cells(lastRow, "B").Value = Format(Now, "yyyy-MM-dd hh:mm:ss")
+    indexSheet.Hyperlinks.Add Anchor:=indexSheet.Cells(lastRow, "A"), Address:="", SubAddress:= _
+        ws.Name & "!A1", TextToDisplay:=Replace(fileName, ".csv", "")
 
-        'Rename the new sheet to the name of the CSV file
-        Set ws = ActiveSheet
-        ws.Name = Replace(fileName, ".csv", "")
-
-        'Add the date and time the file was imported to the index sheet
-        lastRow = indexSheet.Cells(indexSheet.Rows.Count, "A").End(xlUp).Row + 1
-        indexSheet.Cells(lastRow, "A").Value = fileName
-        indexSheet.Cells(lastRow, "B").Value = ws.Name
-        indexSheet.Cells(lastRow, "C").Value = Now
-
-        'Move to the next file
-        fileName = Dir()
-    Loop
+    'Move to the next file
+    fileName = Dir()
+Loop
 End Sub
+
+    'code for the add button'
+    Sub AddImportButton()
+    Dim btn As Button
+    Set btn = ThisWorkbook.Sheets(1).Buttons.Add(10, 10, 70, 20)
+    btn.Caption = "Import Data"
+    btn.OnAction = "ImportCSVFiles"
+    btn.Enabled = True
+End Sub
+
